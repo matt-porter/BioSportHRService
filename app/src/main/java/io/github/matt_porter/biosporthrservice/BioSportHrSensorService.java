feb5fbbe1d41f4b3d8a72044b32c63d462707214
Matt Porter <matt_porter@hotmail.co.uk>
2017-03-24 20:50:04 +0000

    Add app code

--- app/src/main/java/io/github/matt_porter/biosporthrservice/BioSportHrSensorService.java
+++ app/src/main/java/io/github/matt_porter/biosporthrservice/BioSportHrSensorService.java
@@ -4,29 +4,63 @@ package io.github.matt_porter.biosporthrservice;
 //import com.google.android.gms.common.*;
 //import com.google.android.gms.common.api.*;
 //import com.google.android.gms.fitness.*;
+import android.content.Context;
+import android.os.RemoteException;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.google.android.gms.fitness.data.*;
 import com.google.android.gms.fitness.service.*;
 
+import com.intel.heartratecore.api.HeartRateCore;
+import com.intel.heartratecore.api.HeartRateCoreListener;
+import com.intel.heartratecore.api.HeartRateCoreStatus;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by matt on 07/09/16.
  */
 
-public class BioSportHrSensorService extends FitnessSensorService {
+public class BioSportHrSensorService extends FitnessSensorService implements HeartRateCoreListener {
+    private HeartRateCore hrc;
+    private FitnessSensorServiceRequest request;
+    private String tag = "BioSportHrSensorService";
+    private DataSource datasource = new DataSource.Builder()
+            .setDataType(DataType.TYPE_HEART_RATE_BPM)
+            .setType(DataSource.TYPE_RAW)
+            .build();
+
+    /*
+        Google fit/sensor stuff first.
+     */
+
     @Override
     public void onCreate() {
+        Log.d(this.tag, "onCreate");
         super.onCreate();
+        this.hrc = HeartRateCore.getInstance(this, this);
+        Log.d(this.tag, "starting in onCreate");
+        this.hrc.start();
+        Log.d(this.tag, "Done onCreate");
         // 1. Initialize your software sensor(s).
         // 2. Create DataSource representations of your software sensor(s).
         // 3. Initialize some data structure to keep track of a registration for each sensor.
+
     }
 
     @Override
     public List<DataSource> onFindDataSources(List<DataType> dataTypes) {
+        Log.d(this.tag, "onFindDataSources");
+
         // 1. Find which of your software sensors provide the data types requested.
         // 2. Return those as a list of DataSource objects.
-        return null;
+        List<DataSource> dataSources = new ArrayList<>();
+        dataSources.add(this.datasource);
+        return dataSources;
     }
 
     @Override
@@ -37,6 +71,13 @@ public class BioSportHrSensorService extends FitnessSensorService {
         // 4. Configure your sensor according to the request parameters.
         // 5. When the sensor has new data, deliver it to the platform by calling
         //    request.getDispatcher().publish(List<DataPoint> dataPoints)
+        Log.d(this.tag, "onRegister");
+        if (request.getDataSource() == this.datasource) {
+            Log.d(this.tag, "Starting...");
+            this.hrc.start();
+            this.request = request;
+        }
+        Log.d(this.tag, "Done onRegister");
         return true;
     }
 
@@ -44,7 +85,35 @@ public class BioSportHrSensorService extends FitnessSensorService {
     public boolean onUnregister(DataSource dataSource) {
         // 1. Configure this sensor to stop delivering data to the platform
         // 2. Discard the reference to the registration request object
+        Log.d(this.tag, "onUnregister::"+ dataSource.toString());
+        this.hrc.stop();
+        this.request = null;
         return true;
+    }
+
+    /*
+     * Intel related stuff below
+     */
+
+    @Override
+    public void onStatusChanged(@NonNull EnumSet<HeartRateCoreStatus> enumSet) {
+        Log.d(this.tag, "onStatusChanged::" + enumSet.toString());
+    }
+
+    @Override
+    public void onHeartRateChanged(int i) {
+        Log.d(this.tag, "Heart rate changed: " + i);
+        DataPoint dp = DataPoint.create(this.datasource);
+        dp.setTimestamp(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        dp.getValue(Field.FIELD_BPM).setFloat((float)i);
+        List<DataPoint> dataPoints = new ArrayList();
+        dataPoints.add(dp);
+//        try {
+//            this.request.getDispatcher().publish(dataPoints);
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
+        Log.d(this.tag, "Done HR change");
     }
 }
 
--- app/src/main/java/io/github/matt_porter/biosporthrservice/HRView.java
+++ app/src/main/java/io/github/matt_porter/biosporthrservice/HRView.java
@@ -1,36 +1,161 @@
 package io.github.matt_porter.biosporthrservice;
 
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.result.DataSourcesResult;
 import com.intel.heartratecore.api.HeartRateCore;
 import com.intel.heartratecore.api.HeartRateCoreListener;
 import com.intel.heartratecore.api.HeartRateCoreStatus;
 
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
+
 public class HRView extends AppCompatActivity {
+
+    final String TAG = "HRView";
+    OnDataPointListener mListener = null;
+    GoogleApiClient mClient = null;
+    SensorManager mSensorManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.SENSORS_API)
+                .build();
         setContentView(R.layout.activity_hrview);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Intent serviceIntent = new Intent(this, BioSportHrSensorService.class);
+        ComponentName cn = this.startService(serviceIntent);
+        Log.d(TAG, cn.toString());
         FloatingActionButton start = (FloatingActionButton) findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.KITKAT_WATCH)
             @Override
             public void onClick(View view) {
                 // Pops up a little message...
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Log.d(TAG, "onClick");
+                Snackbar.make(view, "Looking for sources", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.d(TAG, "HR Sensors: " + mSensorManager.getSensorList(Sensor.TYPE_HEART_RATE).toString());
+
+                SensorRequest sr = new SensorRequest.Builder()
+                        .setDataType(DataType.TYPE_HEART_RATE_BPM)
+                        .setSamplingRate(10, TimeUnit.SECONDS)  // sample every 10s
+                        .build();
+
+                mListener = new OnDataPointListener() {
+                    @Override
+                    public void onDataPoint(DataPoint dataPoint) {
+                        for (Field field : dataPoint.getDataType().getFields()) {
+                            Value val = dataPoint.getValue(field);
+                            Log.i(TAG, "Detected DataPoint field: " + field.getName());
+                            Log.i(TAG, "Detected DataPoint value: " + val);
+                        }
+                    }
+                };
+
+                PendingResult<Status> result = Fitness.SensorsApi.add(
+                        mClient,
+                        sr,
+                        mListener);
+                Log.i(TAG, result.toString());
+                result.setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        Log.i(TAG, "Result received " + status.toString());
+                    }
+                });
+                Fitness.SensorsApi.findDataSources(mClient, new DataSourcesRequest.Builder()
+                    // At least one datatype must be specified.
+                    .setDataTypes(DataType.TYPE_HEART_RATE_BPM)
+                    // Can specify whether data type is raw or derived.
+                    .setDataSourceTypes(DataSource.TYPE_RAW)
+                    .build())
+                    .setResultCallback(new ResultCallback<DataSourcesResult>() {
+                        @Override
+                        public void onResult(DataSourcesResult dataSourcesResult) {
+                            Log.d(TAG, "Result: " + dataSourcesResult.getStatus().toString());
+                            for (DataSource dataSource : dataSourcesResult.getDataSources()) {
+                                Log.d(TAG, "Data source found: " + dataSource.toString());
+                                Log.d(TAG, "Data Source type: " + dataSource.getDataType().getName());
+
+                                //Let's register a listener to receive Activity data!
+                                if (dataSource.getDataType().equals(DataType.TYPE_HEART_RATE_BPM)
+                                        && mListener == null) {
+                                    Log.i(TAG, "Data source for TYPE_HEART_RATE_BPM found!  Registering.");
+                                    registerFitnessDataListener(dataSource,
+                                            DataType.TYPE_HEART_RATE_BPM);
+                                }
+                            }
+                        }
+                    });
             }
         });
+    }
+
+    private boolean registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+        mListener = new OnDataPointListener() {
+            @Override
+            public void onDataPoint(DataPoint dataPoint) {
+                for (Field field : dataPoint.getDataType().getFields()) {
+                    Value val = dataPoint.getValue(field);
+                    Log.i(TAG, "Detected DataPoint field: " + field.getName());
+                    Log.i(TAG, "Detected DataPoint value: " + val);
+                }
+            }
+        };
+
+        Fitness.SensorsApi.add(
+                mClient,
+                new SensorRequest.Builder()
+                        .setDataSource(dataSource) // Optional but recommended for custom data sets.
+                        .setDataType(dataType) // Can't be omitted.
+                        .setSamplingRate(1, TimeUnit.SECONDS)
+                        .build(),
+                mListener)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            Log.i(TAG, "Listener registered!");
+                        } else {
+                            Log.i(TAG, "Listener not registered.");
+                        }
+                    }
+                });
+        return true;
     }
 
     @Override
